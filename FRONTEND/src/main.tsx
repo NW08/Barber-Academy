3044738e29286ba65bd75713fbfa9e79c1d113a0
Josué Ortiz <168014672+NW08@users.noreply.github.com>
2025-12-01 13:15:44 -0500

    Setup Fixed

--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -1,14 +1,17 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {createRoot} from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
-import { AuthProvider } from "./context/AuthContext";
+import {AuthProvider} from "./context/AuthContext";
+import {StrictMode} from "react";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    {/* Envolver la App con el Provider */}
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      {/* Envolver la App con el Provider */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>,
 );
